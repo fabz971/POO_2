357977fad31c495b1230a3711034d817de911098
fabz971 <fabz971@gmail.com>
2017-11-02 14:40:24 -0300

    Se creo la clase main con un contador en reversa y print

---
+++ TestGithub/src/main/Main.java
@@ -0,0 +1,17 @@
+
+package main;
+
+/**
+ *
+ * @author fbarrera
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Morir en : "+i);
+        }
+    }
+    
+}
